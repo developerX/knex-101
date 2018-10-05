fc16925dfc5358061d30c23b9a7c6c2a5ef9b660
Mansoor Bahramand <mansoor.bahramand@gMB96db97.local>
2018-10-05 15:10:37 -0700

    added awards endpoint to return all awards from the db

--- routes/awards.js
+++ routes/awards.js
@@ -1,8 +1,20 @@
+const env = 'development';
 const express = require('express');
 const router = express.Router();
+const config = require('../knexconfig.js')[env];
+const knex = require('knex')(config);
 
 router.get('/', (request, response) => {
-  response.send(" I am the Awards page");
+  knex('awards')
+    .then(results => {
+      response.json(results);
+      knex.destroy();
+    })
+    .catch(err => {
+      response.error(err)
+      knex.destroy();
+      process.exit(1);
+    });
 });
 
 module.exports = router;
